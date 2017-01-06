61816609ec8aad7fbf02cf3328afa9e4c1300700
Carmine Tramontano <carminetramontano0@gmail.com>
2017-01-06 19:54:41 +0100

    Update Noleggio.java

--- AC/src/bean/Noleggio.java
+++ AC/src/bean/Noleggio.java
@@ -1,5 +1,47 @@
 package bean;
 
-public class Noleggio {
+public class Noleggio
+{
+	public Noleggio()
+	{
+		codice = -1;
+		datainizio = "";
+		datafine = "";
+	}
+	
 
+	public int getCodice() 
+	{
+		return codice;
+	}
+	
+	public void setCodice(int codice) 
+	{
+		this.codice = codice;
+	}
+	
+	public Date getDatainizio() 
+	{
+		return datainizio;
+	}
+	
+	public void setDatainizio(Date datainizio) 
+	{
+		this.datainizio = datainizio;
+	}
+	
+	public Date getDatafine() 
+	{
+		return datafine;
+	}
+	
+	public void setDatafine(Date datafine) 
+	{
+		this.datafine = datafine;
+	}
+	
+	private int codice;
+	private Date datainizio;
+	private Date datafine;
+	
 }
