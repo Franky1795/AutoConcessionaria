a4e6f701c5f3db3221bc7bfb188cf96610ae606a
Carmine Tramontano <carminetramontano0@gmail.com>
2017-01-06 19:49:43 +0100

    Update Newsletter.java

--- AC/src/bean/Newsletter.java
+++ AC/src/bean/Newsletter.java
@@ -1,5 +1,56 @@
 package bean;
 
-public class Newsletter {
+public class Newsletter
+{
+	public Newsletter()
+	{
+		codice = -1;
+		nome = "";
+		cognome = "";
+		email = "";
+	}
+	
+	public int getCodice()
+	{
+		return codice;
+	}
+	
+	public void setCodice(int codice)
+	{
+		this.codice = codice;
+	}
+	
+	public String getNome()
+	{
+		return nome;
+	}
+	
+	public void setNome(String nome)
+	{
+		this.nome = nome;
+	}
+	
 
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+	private int codice;
+	private String nome;
+	private String cognome;
+	private String email;
+	
 }
